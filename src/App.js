2896d8dead1c1c137bd409af929701706c2716ef
MmSARGIN <melih.sargin.53@gmail.com>
2022-08-22 10:17:53 +0300

    BODY

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     const requestOptions = {
         method: 'GET',
         headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*'},
-        body: JSON.stringify({ title: 'React POST Request Example' })
+       
     };
     fetch('https://login.salesforce.com/services/oauth2/token?client_id=3MVG9DREgiBqN9Wm8HeFOlyPkPgGiLzuZ4b5LlN01rbrqFIZyVVhg5PX4zDbpb7Y6DG05BShL7L6lRj5rLlBe&client_secret=213192E46626074BDE44DAE7FE07C1C551D229FEBDE184F30148B98FEC1F187B&password=melih.5377Y6lc98TAfNuGY8RLv5UfHKLj&username=ensonhesap@salesforce.com&grant_type=password', requestOptions, { mode: 'no-cors'})
         .then(response => response.json())
