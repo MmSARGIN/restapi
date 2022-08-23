be076d431c0fa138fac7be3bc1ab6f351e7230bb
MmSARGIN <melih.sargin.53@gmail.com>
2022-08-23 12:19:41 +0300

    account

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         
        
     };
-    fetch('https://mms-b-dev-ed.my.salesforce.com/services/data/v55.0/query?q=SELECT+Name+FROM+Account', requestOptions)
+    fetch('https://mms-b-dev-ed.my.salesforce.com/services/apexrest/Account/0018d00000B23e4AAB', requestOptions)
         .then(response => response.json())
         .then(data => console.log(data))
         // .then(data => this.setState({ postId: data.id }));
