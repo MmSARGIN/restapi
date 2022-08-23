0fdd63aef5a2b5a147698d417ddc0904a799260c
MmSARGIN <melih.sargin.53@gmail.com>
2022-08-23 10:56:47 +0300

    url

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ function App() {
   // Simple POST request with a JSON body using fetch
     const requestOptions = {
         method: 'GET',
-        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*'},
+        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*'}, 'Authorization' : 'Bearer 00D8d000004KV9a!AQkAQL8KjFpUpPhQfiFYwmP4R7qoMODr0aNhJSpVwCxGqlFV3Lreic_PDMZ_Ojbv6FE7w40uMH.FtEQqox28nu64ICoMgVlq',
+        
        
     };
-    fetch('https://login.salesforce.com/services/oauth2/token?client_id=3MVG9DREgiBqN9Wm8HeFOlyPkPgGiLzuZ4b5LlN01rbrqFIZyVVhg5PX4zDbpb7Y6DG05BShL7L6lRj5rLlBe&client_secret=213192E46626074BDE44DAE7FE07C1C551D229FEBDE184F30148B98FEC1F187B&password=melih.5377Y6lc98TAfNuGY8RLv5UfHKLj&username=ensonhesap@salesforce.com&grant_type=password', requestOptions, { mode: 'no-cors'})
+    fetch('https://mms-b-dev-ed.my.salesforce.com/services/data/v55.0/query?q=SELECT+Name+FROM+Account', requestOptions, { mode: 'no-cors'})
         .then(response => response.json())
         .then(data => this.setState({ postId: data.id }));
 
